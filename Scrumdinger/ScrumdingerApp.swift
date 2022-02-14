c78ceb41b7eb2526e5815b0355ba0a1a2d73d5c3
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-02-14 20:17:16 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Gavin Henderson on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
