1724e1b977dbb364fb5bc892cf9632a221b86795
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-02-21 21:45:42 +0000

    broken version

--- Scrumdinger/Models/AVPlayer+Ding.swift
+++ Scrumdinger/Models/AVPlayer+Ding.swift
@@ -1,5 +1,5 @@
-import Foundation
 import AVFoundation
+import Foundation
 
 extension AVPlayer {
     static let sharedDingPlayer: AVPlayer = {
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -2,8 +2,10 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State var isPresentingNewScrumView = false
     @State var newScrumData = DailyScrum.Data()
+    let saveAction: ()->Void
     
     var body: some View {
         List {
@@ -44,13 +46,16 @@ struct ScrumsView: View {
                     }
             }
         }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+        }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            ScrumsView(scrums: .constant(DailyScrum.sampleData))
+            ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction:{})
         }
     }
 }
